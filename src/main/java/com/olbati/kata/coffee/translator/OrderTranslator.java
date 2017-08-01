77e9bb7786abe78f0d8fd2ea63156864279b1106
Ahmed Jerid <ahmed.jerid@olbati.com>
2017-08-01 09:51:15 +0200

    Init commit

---
+++ src/main/java/com/olbati/kata/coffee/translator/OrderTranslator.java
@@ -0,0 +1,14 @@
+package com.olbati.kata.coffee.translator;
+
+import com.olbati.kata.coffee.domain.Order;
+
+/**
+ * Hello world!
+ */
+public class OrderTranslator {
+
+
+
+
+
+}
---
+++ src/test/java/com/olbati/kata/coffee/OrderTranslatorTest.java
@@ -0,0 +1,18 @@
+package com.olbati.kata.coffee;
+
+import com.olbati.kata.coffee.domain.Order;
+import com.olbati.kata.coffee.translator.OrderTranslator;
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertFalse;
+
+/**
+ * Unit test for simple OrderTranslator.
+ */
+public class OrderTranslatorTest {
+
+
+
+
+}
